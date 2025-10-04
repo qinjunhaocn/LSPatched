81d4abdbae60a744808cbe2cb85b17a8ab005c4b
qinjunhaocn <avf_j7b6@163.com>
2025-10-04 21:03:36 +0800

    hybrid

---
+++ LSPatch/manager/src/main/java/org/lsposed/lspatch/util/ShizukuApi.kt
@@ -0,0 +1,81 @@
+package org.lsposed.lspatch.util
+
+import android.content.IntentSender
+import android.content.pm.*
+import android.os.Build
+import android.os.IBinder
+import android.os.IInterface
+import android.os.Process
+import android.os.SystemProperties
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import dev.rikka.tools.refine.Refine
+import rikka.shizuku.Shizuku
+import rikka.shizuku.ShizukuBinderWrapper
+import rikka.shizuku.SystemServiceHelper
+
+object ShizukuApi {
+
+    private fun IBinder.wrap() = ShizukuBinderWrapper(this)
+    private fun IInterface.asShizukuBinder() = this.asBinder().wrap()
+
+    private val iPackageManager: IPackageManager by lazy {
+        IPackageManager.Stub.asInterface(SystemServiceHelper.getSystemService("package").wrap())
+    }
+
+    private val iPackageInstaller: IPackageInstaller by lazy {
+        IPackageInstaller.Stub.asInterface(iPackageManager.packageInstaller.asShizukuBinder())
+    }
+
+    private val packageInstaller: PackageInstaller by lazy {
+        val userId = Process.myUserHandle().hashCode()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            Refine.unsafeCast(PackageInstallerHidden(iPackageInstaller, "com.android.shell", null, userId))
+        } else {
+            Refine.unsafeCast(PackageInstallerHidden(iPackageInstaller, "com.android.shell", userId))
+        }
+    }
+
+    var isBinderAvailable = false
+    var isPermissionGranted by mutableStateOf(false)
+
+    fun init() {
+        Shizuku.addBinderReceivedListenerSticky {
+            isBinderAvailable = true
+            isPermissionGranted = Shizuku.checkSelfPermission() == PackageManager.PERMISSION_GRANTED
+        }
+        Shizuku.addBinderDeadListener {
+            isBinderAvailable = false
+            isPermissionGranted = false
+        }
+    }
+
+    fun createPackageInstallerSession(params: PackageInstaller.SessionParams): PackageInstaller.Session {
+        val sessionId = packageInstaller.createSession(params)
+        val iSession = IPackageInstallerSession.Stub.asInterface(iPackageInstaller.openSession(sessionId).asShizukuBinder())
+        return Refine.unsafeCast(PackageInstallerHidden.SessionHidden(iSession))
+    }
+
+    fun isPackageInstalledWithoutPatch(packageName: String): Boolean {
+        val userId = Process.myUserHandle().hashCode()
+        val app = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            iPackageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA.toLong(), userId)
+        } else {
+            iPackageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA, userId)
+        }
+        return (app != null) && (app.metaData?.containsKey("lspatch") != true)
+    }
+
+    fun uninstallPackage(packageName: String, intentSender: IntentSender) {
+        packageInstaller.uninstall(packageName, intentSender)
+    }
+
+    fun performDexOptMode(packageName: String): Boolean {
+        return iPackageManager.performDexOptMode(
+            packageName,
+            SystemProperties.getBoolean("dalvik.vm.usejitprofiles", false),
+            "verify", true, true, null
+        )
+    }
+}
